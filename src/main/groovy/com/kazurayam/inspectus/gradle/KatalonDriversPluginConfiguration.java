0bb2fa61f07a7fa954773231674b879d769a6da2
kazurayam <kazuaki.urayama@gmail.com>
2022-12-11 12:22:41 +0900

    #18; coined 0.2.2

--- src/main/groovy/com/kazurayam/inspectus/gradle/KatalonDriversPluginConfiguration.java
+++ src/main/groovy/com/kazurayam/inspectus/gradle/KatalonDriversPluginConfiguration.java
@@ -2,7 +2,7 @@ package com.kazurayam.inspectus.gradle;
 
 public class KatalonDriversPluginConfiguration {
 
-    public String inspectusVersion = "0.6.0";
+    public String inspectusVersion = "0.7.2";
 
     public String ExecutionProfilesLoaderVersion = "1.2.1";
 
