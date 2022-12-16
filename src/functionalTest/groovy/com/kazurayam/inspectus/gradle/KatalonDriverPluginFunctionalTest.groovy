78e2dea41e161d198d148d15d75bf274513ba8a5
kazurayam <kazuaki.urayama@gmail.com>
2022-12-16 17:29:59 +0900

    #27

--- src/functionalTest/groovy/com/kazurayam/inspectus/gradle/KatalonDriverPluginFunctionalTest.groovy
+++ src/functionalTest/groovy/com/kazurayam/inspectus/gradle/KatalonDriverPluginFunctionalTest.groovy
@@ -19,4 +19,34 @@ class KatalonDriverPluginFunctionalTest extends Specification {
         """
     }
 
+    /**
+     * test if we can specify a value for the materialstoreVersion property
+     * @return
+     */
+    def "test drivers task"() {
+        /*
+        buildFile << """
+           drivers {
+               inspectusVersion = "0.8.2"
+           }
+        """
+         */
+        when:
+        def result = GradleRunner.create()
+                .withProjectDir(testProjectDir)
+                .withArguments('drivers')
+                .withPluginClasspath()
+                .build()
+        then:
+        result.output.contains("inspectus")
+        result.output.contains("ExecutionProfilesLoader")
+        result.output.contains("materialstore")
+        result.output.contains("ashot")
+        result.output.contains("commons-csv")
+        result.output.contains("jsoup")
+        result.output.contains("freemarker")
+        result.output.contains("java-diff-utils")
+        result.task(":drivers").outcome == SUCCESS
+    }
+
 }
