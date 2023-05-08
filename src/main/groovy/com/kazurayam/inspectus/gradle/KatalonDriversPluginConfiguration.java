38c7913e6db2bdd2c0845a8918ca4a762dd53485
kazurayam <kazuaki.urayama@gmail.com>
2023-05-08 10:28:55 +0900

    #48

--- src/main/groovy/com/kazurayam/inspectus/gradle/KatalonDriversPluginConfiguration.java
+++ src/main/groovy/com/kazurayam/inspectus/gradle/KatalonDriversPluginConfiguration.java
@@ -2,11 +2,11 @@ package com.kazurayam.inspectus.gradle;
 
 public class KatalonDriversPluginConfiguration {
 
-    public String inspectusVersion = "0.9.3";
+    public String inspectusVersion = "0.9.4";
 
     public String ExecutionProfilesLoaderVersion = "2.1.0";
 
-    public String sampleProjectVersion = "0.4.3";
+    public String sampleProjectVersion = "0.4.4";
 
 
 }
