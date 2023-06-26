6d280647296887697cd38a485f3c39abf343a08e
kazurayam <kazuaki.urayama@gmail.com>
2023-06-26 14:47:26 +0900

    coined v0.5.0; use inspectus-0.10.0

--- src/main/groovy/com/kazurayam/inspectus/gradle/KatalonDriversPluginConfiguration.java
+++ src/main/groovy/com/kazurayam/inspectus/gradle/KatalonDriversPluginConfiguration.java
@@ -2,11 +2,10 @@ package com.kazurayam.inspectus.gradle;
 
 public class KatalonDriversPluginConfiguration {
 
-    public String inspectusVersion = "0.9.5";
+    public String inspectusVersion = "0.10.0";
 
     public String ExecutionProfilesLoaderVersion = "2.1.0";
 
-    public String sampleProjectVersion = "0.4.5";
-
+    public String sampleProjectVersion = "0.5.0";
 
 }
